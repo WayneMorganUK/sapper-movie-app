2f1fe1fb64f9f4dd6bedae0ddb3fa3d3c4b4cb6d
WormCrew <44246461+WormCrew@users.noreply.github.com>
2021-03-28 23:33:13 +0100

    updates

--- src/components/fetchData.ts
+++ src/components/fetchData.ts
@@ -1,12 +1,5 @@
 export async function getMovieDetails(url) {
   const res = await fetch(url)
-  if(res.status === 200 ){
-    const movieResult = await res.json()
-    return movieResult
-  }
-  if(res.status === 404 ){
-    const { error } = await res.json()
-    this.error(404,error)
-
-  }
+  const movieResult = await res.json()
+  return movieResult
 }
--- src/components/fetchTrailer.ts
+++ src/components/fetchTrailer.ts
@@ -1,12 +1,6 @@
 export async function getTrailerId(VIDEO_API) {
   const res = await fetch(VIDEO_API)
-  if(res.status === 200 ){
-    const preview = await res.json()
-    const preview_Id = preview.results.length ?  preview.results[0].key : '999'
-    return preview_Id
-  }
-  if(res.status === 404 ){
-    const { error } = await res.json()
-    this.error(404,error)
-  }
+  const preview = await res.json()
+  const preview_Id = preview.results.length ?  preview.results[0].key : '999'
+  return preview_Id
 }
